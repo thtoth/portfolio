8594ff3d7be18d5fe713c5b5ec8551c6a11d31b5
thtoth <ttoth@juniper.net>
2021-07-08 08:15:32 -0700

    final project except for seo config

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,24 @@ import socialLinks from '../constants/social_links';
 
 const Footer = () => {
 	return (
-		<h2>footer component</h2>
+		<footer className="footer">
+			<div>
+				<div className="footer-links social-links">
+					{socialLinks.map((link) => {
+						return (
+							<a 
+								href={link.url} 
+								key={link.id}
+								className="social-link"
+							>
+								{link.icon}
+							</a>
+						);
+					})}
+				</div>
+				<h4>copyright &copy; {new Date().getFullYear()}<span> Langley Labs </span> all rights reservered </h4>
+			</div>
+		</footer>
 	);
 }
 
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -4,7 +4,19 @@ import Seo from '../components/Seo';
 const Contact = () => {
 	return (
 		<>
-			<h2>contact page</h2>
+			<section className="contact-page">
+				<article className="contact-form">
+					<h3>get in touch</h3>
+					<form>
+						<div className="form-group">
+							<input type="text" name="name" placeholder="name" className="form-control"/>
+							<input type="email" name="email" placeholder="email" className="form-control"/>
+							<textarea name="message" rows="5" placeholder="message" className="form-control"/>
+						</div>
+						<button type="submit" className="submit-btn btn">submit here</button>
+					</form>
+				</article>
+			</section>
 		</>
 	);
 }
